d5ca8101449193a83f4b8695692088305af68ad0
Matt Mitchell <matt.mitchell@nrel.gov>
2022-10-14 21:47:10 -0600

    more cli

--- scp/cli.py
+++ scp/cli.py
@@ -13,22 +13,22 @@ _valid_fluid_strs = ["EA", "EG", "MA", "PG", "W"]
 
 
 def _check_valid_prop(prop_str: str):
-    if prop_str.upper() not in _valid_prop_strs:
+    if prop_str not in _valid_prop_strs:
         msg = f"PropStr not valid: {prop_str}"
         raise ValueError(msg)
 
 
 def _check_valid_fluid(fluid_str: str):
-    if fluid_str.upper() not in _valid_fluid_strs:
+    if fluid_str not in _valid_fluid_strs:
         msg = f"FluidStr not valid: {fluid_str}"
         raise ValueError(msg)
 
 
-def SCProps(args):
-    prop_str = args.PropStr
-    temp = args.Temp
-    x_conc = args.XConc
-    fluid_str = args.FluidStr
+def SCProps(prop_str, temp, x_conc, fluid_str):
+
+    prop_str = prop_str.upper()
+    fluid_str = fluid_str.upper()
+
     _check_valid_prop(prop_str)
     _check_valid_fluid(fluid_str)
 
@@ -47,16 +47,16 @@ def SCProps(args):
         raise NotImplementedError("Should never get here")
 
     if prop_str == "V":
-        print(inst.viscosity(temp))
+        return inst.viscosity(temp)
 
     if prop_str == "C":
-        print(inst.specific_heat(temp))
+        return inst.specific_heat(temp)
 
     if prop_str == "D":
-        print(inst.density(temp))
+        return inst.density(temp)
 
     if prop_str == "L":
-        print(inst.conductivity(temp))
+        return inst.conductivity(temp)
 
 
 def main():
@@ -66,7 +66,7 @@ def main():
     parser.add_argument("XConc", type=float, help="Concentration fraction: [0-1]")
     parser.add_argument("FluidStr", type=str, help=f"Fluid strings: {_valid_fluid_strs}")
     args = parser.parse_args()
-    SCProps(args)
+    print(SCProps(args.PropStr, args.Temp, args.XConc, args.FluidStr))
 
 
 if __name__ == "__main__":
