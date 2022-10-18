ea3e5b7ba197216cfc5dbce93f1a747abd41e34b
j-c-cook <jack-c-cook@protonmail.com>
2022-10-18 18:37:28 -0500

    Add fluid handler object

---
+++ scp/fluid.py
@@ -0,0 +1,25 @@
+from . import propylene_glycol
+from . import ethylene_glycol
+from . import methyl_alcohol
+from . import ethyl_alcohol
+from . import water
+
+
+class Fluid:
+
+    fluids = {
+        ('WATER',): water.Water,
+        ('MPG', 'PROPYLENEGLYCOL'): propylene_glycol.PropyleneGlycol,
+        ('MEG', 'ETHYLENEGLYCOL'): ethylene_glycol.EthyleneGlycol,
+        ('MMA', 'METHYLALCOHOL'): methyl_alcohol.MethylAlcohol,
+        ('ETHYLENEGLYCOL', 'MEA'): ethyl_alcohol.EthylAlcohol
+    }
+
+    @staticmethod
+    def __new__(cls, fluid_name, x, *args, **kwargs):
+
+        for key in Fluid.fluids:
+            if fluid_name.upper() in key:
+                return Fluid.fluids[key](0.0)
+        else:
+            raise ValueError("Unknown fluid type.")
